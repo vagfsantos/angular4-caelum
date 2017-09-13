6d87535433432dc1c4f572971186777774b45d16
vagner <santos.vagnerferreira@gmail.com>
2017-09-12 22:49:45 -0300

    second class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Caelum Pic';
+
+  constructor(http: Http) {
+
+  }
 }
